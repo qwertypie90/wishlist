074735527379927ce7b4abdc8d0c9da338f6ab91
qwertypie90 <lindseymcd@me.com>
2023-01-04 13:19:44 -0800

    added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,21 @@ import Home from './pages/Home';
 import Post from './pages/Post';
 import Login from './pages/Login';
 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 const App = () => {
+  const user = false;
   return (
-    <div>
-      <Navbar />
-      <Login />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+          <Route path="/post/:id" element={user ? <Post /> : <Navigate to="/login" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
